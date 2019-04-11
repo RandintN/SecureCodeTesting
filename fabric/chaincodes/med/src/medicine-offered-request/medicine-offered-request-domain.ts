e6011528a09edb9cc40051ca592b4f082e950b14
vpoliveira <vpoliveira@cpqd.com.br>
2019-04-11 10:02:38 -0300

    [MAT-261] Approve Offer Medicine Request
    - Chaincode approve offer medicine request;

--- fabric/chaincodes/med/src/medicine-offered-request/medicine-offered-request-domain.ts
+++ fabric/chaincodes/med/src/medicine-offered-request/medicine-offered-request-domain.ts
@@ -1,10 +1,12 @@
 import { Context } from 'fabric-contract-api';
-import { ChaincodeResponse } from 'fabric-shim';
+import { ChaincodeResponse, Iterators } from 'fabric-shim';
 import { Guid } from 'guid-typescript';
+
 import { IMedicineOfferJson } from '../medicine-offer/medicine-offer-json';
 import { MedicineOfferedDomain } from '../medicine-offered/medicine-offered-domain';
 import { MedicineOffered } from '../medicine-offered/medicine-offered-model';
 import { IMedicineRequestJson } from '../medicine-request/medicine-request-json';
+import { IMedicineRequestLedgerJson } from '../medicine-request/medicine-request-ledger-json';
 import { ResponseUtil } from '../result/response-util';
 import { Result } from '../result/result';
 import { CommonConstants } from '../utils/common-messages';
@@ -12,6 +14,7 @@ import { DateExtension } from '../utils/date-extension';
 import { MedicineOfferedStatusEnum, MedicineRequestStatusEnum, RequestMode } from '../utils/enums';
 import { ValidationError } from '../validation/validation-error-model';
 import { ValidationResult } from '../validation/validation-model';
+import { IMedicineOfferedApproveJson } from './medicine-offered-approve-json';
 import { IMedicineOfferedRequestService } from './medicine-offered-request-interface';
 import { IMedicineOfferedRequestJson } from './medicine-offered-request-json';
 import { IMedicineOfferedRequestLedgerJson } from './medicine-offered-request-ledger-json';
@@ -38,6 +41,10 @@ export class MedicineOfferedRequestDomain implements IMedicineOfferedRequestServ
     private static ERROR_NEW_RETURN_DATE_EQUAL_RETURN_DATE: ValidationError =
         new ValidationError('MORD-005',
             'The new return date can not equal the medicine request return date.');
+
+    private static ERROR_OFFER_MEDICINE_REQUEST_NOT_FOUND: ValidationError =
+        new ValidationError('MORD-006',
+            'Offer Medicine Request not found.');
 
     public async offerMedicineRequest(ctx: Context, medicineOfferedRequestJson: string): Promise<ChaincodeResponse> {
         try {
@@ -74,6 +81,52 @@ export class MedicineOfferedRequestDomain implements IMedicineOfferedRequestServ
         } catch (error) {
             return ResponseUtil.ResponseError(error.toString(), undefined);
 
+        }
+    }
+
+    /** Check the documentation of IMedicineOfferedRequestService */
+    public async approveOfferMedicineRequest(ctx: Context, approveOfferMedicineRequestJson: string)
+        : Promise<ChaincodeResponse> {
+        try {
+            const medicineOfferedApprove: IMedicineOfferedApproveJson = JSON.parse(approveOfferMedicineRequestJson);
+
+            if (!(await this.existsMedicineRequestStatusApproved(ctx, medicineOfferedApprove.request_id))) {
+                return ResponseUtil.ResponseBadRequest(CommonConstants.VALIDATION_ERROR,
+                    Buffer.from(JSON.stringify(MedicineOfferedRequestDomain.ERROR_MEDICINE_REQUEST_NOT_FOUND)));
+            }
+
+            const medicineOfferedRequests: IMedicineOfferedRequestLedgerJson[]
+                = await this.searchOfferMedicineRequestByRequestId(ctx, medicineOfferedApprove.request_id
+                    , MedicineOfferedStatusEnum.PROPOSED);
+
+            const result = new Result();
+
+            if (medicineOfferedRequests.some((offer) => {
+                return offer.key === medicineOfferedApprove.offer_id;
+            })) {
+                result.rejected_offers = new Array<string>();
+                for (const offer of medicineOfferedRequests) {
+                    if (offer.key === medicineOfferedApprove.offer_id) {
+                        offer.status = MedicineOfferedStatusEnum.ACCEPTED;
+                        result.offer_id = offer.key;
+                    } else {
+                        offer.status = MedicineOfferedStatusEnum.REJECTED;
+                        result.rejected_offers.push(offer.key);
+                    }
+                    await ctx.stub.putState(offer.key
+                        , Buffer.from(JSON.stringify(offer)));
+                }
+
+                result.request_id = medicineOfferedApprove.request_id;
+                result.timestamp = new Date().getTime();
+            } else {
+                return ResponseUtil.ResponseBadRequest(CommonConstants.VALIDATION_ERROR,
+                    Buffer.from(JSON.stringify(MedicineOfferedRequestDomain.ERROR_OFFER_MEDICINE_REQUEST_NOT_FOUND)));
+            }
+            return ResponseUtil.ResponseCreated(Buffer.from(JSON.stringify(result)));
+
+        } catch (error) {
+            return ResponseUtil.ResponseError(error.toString(), undefined);
         }
     }
 
@@ -180,5 +233,63 @@ export class MedicineOfferedRequestDomain implements IMedicineOfferedRequestServ
         }
 
         return true;
+    }
+
+    private async existsMedicineRequestStatusApproved(ctx: Context, requestId: string): Promise<boolean> {
+        const medicineRequestBuffer: Buffer = await ctx.stub.getState(requestId);
+
+        if (medicineRequestBuffer.length === 0) {
+            return false;
+        }
+
+        const medicineRequest: IMedicineRequestLedgerJson = JSON.parse(medicineRequestBuffer.toString());
+        if (medicineRequest.status !== MedicineRequestStatusEnum.APPROVED) {
+            return false;
+        }
+        return true;
+    }
+
+    private async searchOfferMedicineRequestByRequestId(ctx: Context, requestId: string, statusOffer: string)
+        : Promise<IMedicineOfferedRequestLedgerJson[]> {
+
+        // Creates QueryJson of couchDB index query
+        const queryJson = {
+            selector: {
+                request_id: requestId,
+                status: statusOffer,
+            },
+        };
+
+        // Getting query result
+        const iterator: Iterators.StateQueryIterator = await ctx.stub.getQueryResult(JSON.stringify(queryJson));
+        const result: IMedicineOfferedRequestLedgerJson[] = await this.getOfferMedicineByRequesId(iterator);
+        return result;
+    }
+
+    /**
+     * Auxiliar method that's iterate over an interator of offer medicine request to retrieve the query result.
+     * @param iterator iterator
+     */
+    private async getOfferMedicineByRequesId(iterator: Iterators.StateQueryIterator)
+        : Promise<IMedicineOfferedRequestLedgerJson[]> {
+        const medicineOfferedRequestJson: IMedicineOfferedRequestLedgerJson[] =
+            new Array<IMedicineOfferedRequestLedgerJson>();
+        let offer: IMedicineOfferedRequestLedgerJson;
+
+        while (true) {
+            const result = await iterator.next();
+
+            if (result.value && result.value.value.toString()) {
+                offer = JSON.parse(result.value.value.toString('utf8')) as IMedicineOfferedRequestLedgerJson;
+                offer.key = result.value.getKey();
+                medicineOfferedRequestJson.push(offer);
+            }
+
+            if (result.done) {
+                break;
+            }
+
+        }
+        return medicineOfferedRequestJson;
     }
 }
