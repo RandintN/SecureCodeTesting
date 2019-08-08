93d4ee4ed68e756c5a904cba6cca48e097f43287
Eduardo Viola Nicola <enicola@cpqd.com.br>
2019-08-08 16:05:37 -0300

    [MAT-447]Melhorias
    *Padronizar tanto offer_id quanto request_id para virarem id
    *A variável offer_id virou propose_id

--- fabric/chaincodes/med/src/result/result.ts
+++ fabric/chaincodes/med/src/result/result.ts
@@ -1,9 +1,9 @@
 export class Result {
     public timestamp: number;
 // tslint:disable-next-line: variable-name
-    public request_id: string;
+    public id: string;
 // tslint:disable-next-line: variable-name
-    public offer_id: string;
+    public propose_id: string;
 // tslint:disable-next-line: variable-name
     public rejected_offers: string[];
 
