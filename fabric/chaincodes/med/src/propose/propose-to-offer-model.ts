815dc5296083fa87fde57aa7d55153ab07e1d6c4
Eduardo Viola Nicola <enicola@cpqd.com.br>
2019-08-19 14:47:06 -0300

    [MAT-447]Corrige bugs
    -Quando a op não é exchange, o medicine do exchange não é
    inicializado. Isso estava dando erro. Então a inicialização passou
    para a camada de cima, sendo que ele só é populado se tiver algo
    vindo do json.
    
    -Validação do campo de exchange não estava correta dava erro
    de operação inválida mesmo em outras modalidades.

--- fabric/chaincodes/med/src/propose/propose-to-offer-model.ts
+++ fabric/chaincodes/med/src/propose/propose-to-offer-model.ts
@@ -60,6 +60,7 @@ export class ProposeToOffer implements IValidator {
         }
         this.operation = MedicineOperationEnum.OFFER;
         this.exchange = new ProposedExchange();
+        this.exchange.medicine = new MedicineProposeExchange();
         if(proposeToOfferJson.exchange) {
             this.exchange.fromJson(proposeToOfferJson.exchange);
         }
@@ -94,7 +95,7 @@ export class ProposeToOffer implements IValidator {
         if (!this.type) {
             validationResult.addError(ProposeToOffer.ERROR_EMPTY_TYPE);
 
-        } else if (this.type.toLocaleLowerCase() === RequestMode.EXCHANGE && !this.exchange || Object.keys(this.exchange).length === 0) {
+        } else if (this.type.toLocaleLowerCase() === RequestMode.EXCHANGE && (!this.exchange || Object.keys(this.exchange).length === 0 || Object.keys(this.exchange.medicine).length === 0)) {
             validationResult.addError(ProposeToOffer.ERROR_NEGOTIATION_IS_NEEDED);
         }
 
