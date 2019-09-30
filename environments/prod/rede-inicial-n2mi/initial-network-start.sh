c2a60741dc14f94b410f68b0c55b81b991993ca0
Nicholas <nlzanutim@yahoo.com>
2019-09-30 08:56:09 -0300

    [MAT-508] added 2 Raft orderers

--- environments/prod/rede-inicial-n2mi/initial-network-start.sh
+++ environments/prod/rede-inicial-n2mi/initial-network-start.sh
@@ -9,7 +9,7 @@ export PEER_DIRECTORY=./crypto-config/peerOrganizations/peers/peer0.n2med.com
 
 CA_ADDRESS_PORT=rca.n2med.com:7054
 COMAPNY_DOMAIN=n2med.com
-IP_ADDRESS=192.168.65.89
+IP_ADDRESS=192.168.0.8
 
 CONTAINER_IDS=$(docker ps -aq)
 if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
@@ -64,7 +64,7 @@ sudo mv $PEER_DIRECTORY/tls/keystore/*_sk $PEER_DIRECTORY/tls/server.key
 sudo mv $PEER_DIRECTORY/tls/tlscacerts/*.pem $PEER_DIRECTORY/tls/ca.crt
 
 # Delete empty TLS directories
-sudo rm -rf $ORDERER_MSP/tls/{cacerts,keystore,signcerts,tlscacerts,user}
+sudo rm -rf $ORDERER_MSP/orderer.n2med.com/tls/{cacerts,keystore,signcerts,tlscacerts,user}
 sudo rm -rf $ORDERER_MSP/orderer2.n2med.com/tls/{cacerts,keystore,signcerts,tlscacerts,user}
 sudo rm -rf $ORDERER_MSP/orderer3.n2med.com/tls/{cacerts,keystore,signcerts,tlscacerts,user}
 sudo rm -rf $PEER_DIRECTORY/tls/{cacerts,keystore,signcerts,tlscacerts,user}
@@ -75,21 +75,25 @@ sudo cp add-org-n2medchannel.sh ./config
 
 sleep ${FABRIC_START_TIMEOUT}
 
-docker-compose -p n2mi -f docker-compose.yml up -d orderer.n2med.com orderer2.n2med.com orderer3.n2med.com peer0.n2med.com cli couchdb
+docker-compose -p n2mi -f docker-compose.yml up -d peer0.n2med.com cli couchdb
 
-sleep ${FABRIC_START_TIMEOUT}
+docker-compose -p n2mi -f docker-compose.yml up -d orderer.n2med.com 
+docker-compose -p n2mi -f docker-compose.yml up -d orderer2.n2med.com 
+docker-compose -p n2mi -f docker-compose.yml up -d orderer3.n2med.com 
+sleep 7
 
-# Create the channel 
-docker exec -e "CORE_PEER_LOCALMSPID=N2miMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/peerOrganizations/users/Admin@peer0.n2med.com/msp" cli peer channel create -o orderer.n2med.com:7050 -c n2medchannel --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer.n2med.com/tls/ca.crt -f /etc/hyperledger/channel-artifacts/channel.tx
+# Create the channel
+docker exec cli peer channel create -o orderer2.n2med.com:7060 -c n2medchannel --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer2.n2med.com/tls/ca.crt -f /etc/hyperledger/channel-artifacts/channel.tx
 
+docker exec cli peer channel create -o orderer.n2med.com:7050 -c n2medchannel --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer.n2med.com/tls/ca.crt -f /etc/hyperledger/channel-artifacts/channel.tx
 sleep 3
 
 # Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=N2miMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/peerOrganizations/users/Admin@peer0.n2med.com/msp" cli peer channel join -b n2medchannel.block 
+docker exec cli peer channel join -b n2medchannel.block 
 
 sleep ${FABRIC_START_TIMEOUT}
 
-docker exec -e "CORE_PEER_LOCALMSPID=N2miMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/peerOrganizations/users/Admin@peer0.n2med.com/msp" cli peer channel update -o orderer.n2med.com:7050 --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer.n2med.com/tls/ca.crt -c n2medchannel -f /etc/hyperledger/channel-artifacts/N2miMSPanchors.tx
+docker exec cli peer channel update -o orderer2.n2med.com:7060 -c n2medchannel --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer2.n2med.com/tls/ca.crt -f /etc/hyperledger/channel-artifacts/N2miMSPanchors.tx
 
 pushd ../../../fabric/chaincodes/med
 npm install
@@ -98,7 +102,7 @@ popd
 
 docker exec -e "CORE_PEER_LOCALMSPID=N2miMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/peerOrganizations/users/Admin@peer0.n2med.com/msp" cli peer chaincode install -n med -v 1 -p /opt/med/ -l node
 
-docker exec -e "CORE_PEER_LOCALMSPID=N2miMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/peerOrganizations/users/Admin@peer0.n2med.com/msp" cli peer chaincode instantiate -o orderer.n2med.com:7050 -C n2medchannel -n med -l node -v 1 -c '{"Args":[]}' -P "OR('N2miMSP.member')" --collections-config /opt/med/collection-config.json --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer.n2med.com/tls/ca.crt
+docker exec -e "CORE_PEER_LOCALMSPID=N2miMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/peerOrganizations/users/Admin@peer0.n2med.com/msp" cli peer chaincode instantiate -o orderer2.n2med.com:7060 -C n2medchannel -n med -l node -v 1 -c '{"Args":[]}' -P "OR('N2miMSP.member')" --collections-config /opt/med/collection-config.json --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer2.n2med.com/tls/ca.crt
 
 docker exec cli peer chaincode package -n med -p /opt/med -v 1 medcc.pak -l node
 sudo chmod -R 777 ./crypto-config/medcc.pak
@@ -107,5 +111,15 @@ sudo chmod -R 777 ./n2medCa
 cp ./n2medCa/tls-cert.pem ./artifacts
 cp ./crypto-config/medcc.pak ./artifacts
 cp ./crypto-config/ordererOrganizations/orderers/orderer.n2med.com/tls/server.crt ./artifacts
+
+# sleep 3 
+
+# docker-compose -p n2mi -f docker-compose.yml up -d orderer2.n2med.com 
+
+# sleep 20
+
+# docker-compose -p n2mi -f docker-compose.yml up -d orderer3.n2med.com 
+
+# sleep 20
 
 #peer chaincode invoke -o orderer.n2med.com:7050  --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer.n2med.com/tls/ca.crt -C n2medchannel -n med -c '{"Args":["addMedicineRequest","{\"amount\":\"23\", \"type\":\"donation\",\"medicine\":{\"active_ingredient\":\"AGUA\", \"pharma_form\":\"Xarope\", \"pharma_industry\":[\"3M\"],\"concentration\":\"33\", \"classification\":[\"Similar\"]},\"id\":\"13\" }"]}'
