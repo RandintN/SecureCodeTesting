f0dfabf26cbb0cc2c24e581b48a13bde85e43723
Zanutim <nzanutim@cpqd.com.br>
2019-10-03 13:16:43 -0300

    [MAT-503] Endpoint Functions

--- environments/prod/nova-org/setup-component/endpoint-connections.js
+++ environments/prod/nova-org/setup-component/endpoint-connections.js
@@ -5,47 +5,37 @@ const fs = require('fs');
 
 exports.send_json = function (orgsJsonName) {
 
-       
-
        var jsonFile = fs.readFileSync(orgsJsonName + '.json');
        var orgJSON = JSON.parse(jsonFile);
 
        request.post(
-              '192.168.68.133:3000/receive-json',
+              '192.168.65.89:3000/receive-json',
               orgJSON,
-              function (error, response, body) {
+              function (error, response) {
                   if (!error && response.statusCode == 200) {
-                      console.log(body);
+                      console.log(response);
                   }
               }
        );
        console.log(org);
 };
 
-exports.request_orderer_genesis = async function (response) {
+exports.request_orderer_genesis = async function () {
        // New org requests orderer genesis
        var requestOrdererGenesis = "Please give me the orderer genesis";
 
        await request.post(
-              '192.168.68.133:3000/receive-json',
+              '192.168.65.89:3000/receive-json',
               requestOrdererGenesis,
               function (error, response, body) {
                   if (!error && response.statusCode == 200) {
                       console.log(body);
                   }
+                     var ordererGenesis = response.body;
+                     fs.writeFileSync('n2medchannel.block', ordererGenesis);
+                     console.log(response);
               }
        );
-       console.log(response);
-
-};
-
-exports.receive_orderer_genesis_new_org = function (ordererGenesis) {
-       // http request orderer from send_orderer_genesis_to_new_org
-       // copy orderer genesis to nova-org/config
-       //execute script docker-compose up -d orderer
-
-       fs.writeFileSync('n2medchannel.block', ordererGenesis);
-
 };
 
 
--- environments/prod/rede-inicial-n2mi/backoffice-onboarding/new-org-endpoints.js
+++ environments/prod/rede-inicial-n2mi/backoffice-onboarding/new-org-endpoints.js
@@ -3,21 +3,24 @@
 const fs = require('fs');
 const exec = require('child_process').exec;
 
-exports.receive_json_new_org = function (orgPeerJson, orgOrdererJson, ordererTLS) {
-      //Type POST
-      
+exports.receive_json_new_org = async function (orgPeerJson, orgOrdererJson, ordererTLS) {
+       //Type POST
+       // Copy files to appropriate folder
+       // execute script inside docker exec cli ./add-org-channel
+       // ping response to new org 
+
        var newOrgPeerJson = orgJson;
        var newOrgOrdererJson = ordererTLS;
        var newOrgOrdererTLS = ordererTLS;
 
-       fs.writeFileSync(orgPeerJson + '.json', newOrgPeerJson);
-       fs.writeFileSync(orgOrdererJson + '.json', newOrgOrdererJson);
-       fs.writeFileSync('server.crt', newOrgOrdererTLS);
+       fs.writeFileSync('../config/' + orgPeerJson + '.json', newOrgPeerJson);
+       fs.writeFileSync('../config/' + orgOrdererJson + '.json', newOrgOrdererJson);
+       fs.writeFileSync('../config/' + 'server.crt', newOrgOrdererTLS);
+
+       exec("docker exec cli sh -c '/etc/hyperledger/channel-artifacts/add-org-n2medchannel.sh " + orgPeerJson + ' ' + orgOrdererJson + "'");
 
        var doneAddingOrg = "We have finished adding the org to the system channels, you may now fetch and join the channel with the peer"
-        // Copy files to appropriate folder
-       // execute script inside docker exec cli ./add-org-channel
-       // ping response to new org 
+
        return doneAddingOrg;
 };
 
@@ -27,21 +30,17 @@ exports.send_orderer_genesis_to_new_org = function () {
        //N2med endpoint for new org to get genesis block
        //Post from New Org and send orderer genesis as  response
 
-       exec('sh generateOrdererGenesis.sh ');
+       exec('docker exec cli peer channel fetch config n2medchannel.block -o orderer.n2med.com:7050 -c n2med-system-channel --tls --cafile /etc/hyperledger/ordererOrganizations/orderers/orderer.n2med.com/tls/ca.crt');
 
-       var n2medOrdererGenesis = fs.readFileSync('../config/n2medchannel.block');;
+       var n2medOrdererGenesis = fs.readFileSync('../config/n2medchannel.block');
 
-       
-           request.post(
-              '192.168.68.133:3000/receive-json',
-              n2medOrdererGenesis,
+       request.post('192.168.68.133:3000/receive-json', n2medOrdererGenesis,
               function (error, response, body) {
               if (!error && response.statusCode == 200) {
                      console.log(body);
                             }
                      }
-              );
-
+       );
 };
 
 
